83222227c6859dd1509992e68b18ecd71e127eca
stevezheng <stevezheng1989@gmail.com>
2015-05-04 02:27:16 +0800

    新增 按件计价界面

--- www/js/item.js
+++ www/js/item.js
@@ -1,13 +1,30 @@
 angular.module('app.item', [])
-  .controller('ItemPieceCtrl', function($scope, $ionicHistory, $yikeUser) {
+  .controller('ItemPieceCtrl', function($scope, $ionicHistory, $location, $yikeUser) {
     $yikeUser.permission();
+    $scope.money = 0;
+    $scope.piece = 0;
+    $scope.tabStatus = 9;
+
+    $scope.addItem = function(value) {
+      $scope.money += value;
+      $scope.piece++;
+    };
+
     $scope.back = function() {
       $ionicHistory.goBack();
-    }
+    };
+
+    $scope.open = function(path) {
+      $location.path(path);
+    };
   })
-  .controller('ItemBagCtrl', function($scope, $ionicHistory, $yikeUser) {
+  .controller('ItemBagCtrl', function($scope, $ionicHistory, $location, $yikeUser) {
     $yikeUser.permission();
     $scope.back = function() {
       $ionicHistory.goBack();
-    }
+    };
+
+    $scope.open = function(path) {
+      $location.path(path);
+    };
   })
