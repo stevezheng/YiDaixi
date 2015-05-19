947fed861ba92ca520d4dcb520dafaa6aeef1f76
stevezheng <stevezheng1989@gmail.com>
2015-05-19 16:41:50 +0800

    新增 广告的增删查

--- server/www/resource/js/admin/ad/ad.add.js
+++ server/www/resource/js/admin/ad/ad.add.js
@@ -10,12 +10,40 @@
   /* @ngInject */
   function AdAddCtrl($scope) {
     $scope.init = init;
+    $scope.submit = submit;
+    $scope.item = {
+      name: ''
+      , price: 0
+      , image: ''
+    };
 
     init();
 
     ////////////////
 
     function init() {
+    }
+
+    function submit() {
+      if (!$scope.item.name) {
+        window.alert('请填写广告名称');
+        return false;
+      }
+
+
+      if (!$scope.item.image) {
+        window.alert('请上传广告图片');
+        return false;
+      }
+
+      D('ad')
+        .add($scope.item)
+        .then(function(res) {
+          window.alert('添加成功');
+          location.reload();
+        }, function(err) {
+          window.alert('添加失败');
+        });
     }
   }
 })();
