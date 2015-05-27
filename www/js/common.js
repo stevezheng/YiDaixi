d420f1a2b811fb1d6bd85ecf43f61010861106a0
stevezheng <stevezheng1989@gmail.com>
2015-05-28 03:26:23 +0800

    更新 首页广告

--- www/js/common.js
+++ www/js/common.js
@@ -4,5 +4,20 @@ angular.module('app.common', [])
     $scope.open= function(target) {
       $location.path(target);
     };
+
+    init();
+
+    function query() {
+      D('ad')
+        .limit(0, 3)
+        .select()
+        .then(function(res) {
+          $scope.ads = res;
+        });
+    }
+
+    function init() {
+      query();
+    }
   });
 
