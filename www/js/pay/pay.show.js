fe408a8fcee156a5902276b8f2d92d65f188a274
stevezheng <stevezheng1989@gmail.com>
2015-05-28 03:20:18 +0800

    新增 充值返回首页

--- www/js/pay/pay.show.js
+++ www/js/pay/pay.show.js
@@ -5,15 +5,16 @@
     .module('pay.show', [])
     .controller('PayShowCtrl', PayShowCtrl);
 
-  PayShowCtrl.$inject = ['$scope', '$ionicPopup'];
+  PayShowCtrl.$inject = ['$scope', '$ionicPopup', '$location'];
 
   /* @ngInject */
-  function PayShowCtrl($scope, $ionicPopup) {
+  function PayShowCtrl($scope, $ionicPopup, $location) {
     $scope.init = init;
     $scope.user = AV.User.current();
     $scope.money = 500;
     $scope.card = '';
     $scope.pay = pay;
+    $scope.open = _open;
     $scope.payCard = payCard;
 
     init();
@@ -37,6 +38,10 @@
 
     function pay() {
       alertPopup('提示', '支付接口申请中');
+    }
+
+    function _open(target) {
+      $location.path(target);
     }
   }
 })();
